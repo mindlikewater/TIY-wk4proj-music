64c66d566ede5f4c66d41238488413cbc134790a
Tai Chen <mindlikewater@gmail.com>
2016-10-22 13:32:21 -0400

    Modified function to display results in HTML

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
 import $ from 'jquery';
 
 import { getTracks } from "./soundcloud_api";
+import { songTemplate, resultsTemplate } from "./templates";
+
+//variable holds location of the div, search-results from HTML page
+var resultsBox = $(".search-results");
+
+//function calls functions that create songs HTML and then displays the results on the webpage
+function displayResults (results) {
+  var playlist = results.map(songTemplate);
+  var html = resultsTemplate(playlist);
+  resultsBox.html(html);
+};
 
 //gets user input from the page, then runs another function to get search results
 function getMusic (event) {
@@ -8,7 +19,10 @@ function getMusic (event) {
   var searchString = $("#search").val();
   var results = getTracks(searchString);
 
-  return results;
-};
+  console.log(searchString);
+  console.log(results);
+  results.then(displayResults);
+}
 
+//connects click event to the submit button on the webpage
 $("#submit").click(getMusic);

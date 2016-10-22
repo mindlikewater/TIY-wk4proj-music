973981329444f7fef8aa9397fcdecc5fc8a56985
Tai Chen <mindlikewater@gmail.com>
2016-10-21 22:38:05 -0400

    Created functions to get ajax data

--- src/js/soundcloud_api.js
+++ src/js/soundcloud_api.js
@@ -1,8 +1,54 @@
 import $ from 'jquery';
+import _ from 'lodash';
+
 import SC_ID from './token';
 
+//global variable holds Soundcloud API source
 var SC_API = "https://api.soundcloud.com";
 
+//used for authentication
 $.ajaxSetup({
-  client_id: `${SC_API}/tracks?client_id=${SC_ID}`
+  data: {
+    client_id: SC_ID
+  }
 });
+
+//function makes ajax request for song data
+function getSongData (searchString) {
+  return $.ajax({
+    url: `${SC_API}/tracks/`,
+    data: {
+      q: searchString,
+      limit: 3
+    }
+  });
+};
+
+function extractData (searchString) {
+  var searchData = searchString.map(function (track) {
+    artwork += track.artwork_url;
+    labelName += track.label_name;
+    releaseYr += release_year;
+    title += track.title;
+    genre += track.genre;
+    api_link += track.uri;
+  });
+  return searchData;
+};
+
+//sorts the song data alphabetically and returns the results in an array
+function generateSongs (songData) {
+  var sorted = _.orderBy(songData, ['title']);
+
+  var results = sorted.map(extractData);
+  return results;
+};
+
+//takes user input from Search button, runs it through request for Soundcloud data,
+//then returns song data
+function getTracks (searchString) {
+  return getSongData(searchString).then(generateSongs);
+};
+
+//export file to main.js
+export { getTracks };

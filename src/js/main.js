888cf3412fb6801b7946df91edc0187df8a13a89
Tai Chen <mindlikewater@gmail.com>
2016-10-21 22:40:43 -0400

    Updated getMusic function on click event

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 import $ from 'jquery';
 
-//var player = $("audio")[0];
+import { getTracks } from "./soundcloud_api";
 
+//gets user input from the page, then runs another function to get search results
 function getMusic (event) {
   event.preventDefault();
+  var searchString = $("#search").val();
+  var results = getTracks(searchString);
 
+  return results;
 };
 
 $("#submit").click(getMusic);

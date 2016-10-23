0d7b8b23541b4581a0ddac7d9df092cafc2abe8d
Tai Chen <mindlikewater@gmail.com>
2016-10-23 17:45:55 -0400

    Added function to update music player with user's selected song

--- src/js/templates.js
+++ src/js/templates.js
@@ -1,21 +1,36 @@
 import $ from 'jquery';
 
-import SC_ID from './token';
-
+//creates html for each individual song
 function songTemplate (track) {
   return `
-    <div class="songbox" audio="${track.trackLink}?client_id=${SC_ID}" format="${track.format}">
+    <div class="songbox" audio="${track.trackLink}" format="${track.format}">
       <image class="artwork" src="${track.artwork}">
       <div class="title">Artist/Title: ${track.title}</div>
       <div class="genre">${track.genre}</div>
     </div>`;
 };
 
+//holds the results of all songs generated from the user's searchString
 function resultsTemplate (tracksHTML) {
   return `
+    <h2 class="search-title">Search Results (click one):</h2>
     <ol class="songs">
       ${tracksHTML.join("")}
     </ol>`;
 };
 
-export { songTemplate, resultsTemplate };
+//updates the music player with the user's choice after user selects a song from the results list
+function play (event) {
+  console.log(event.currentTarget.dataset);
+  var jukeboxHTML = `
+    <audio controls="controls">
+      <source src="${event.currentTarget.dataset.trackLink}" type="audio/${event.currentTarget.dataset.format}">
+    </audio>
+    <div id=$("#currentsong").append.${event.currentTarget.dataset.title}
+    </div>`;
+
+  $(".player").html(jukeboxHTML);
+};
+
+//export functions to main.js
+export { songTemplate, resultsTemplate, play };

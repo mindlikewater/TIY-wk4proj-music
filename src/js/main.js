81b7595907a2103a29d5f74c014fda47a7fc0009
Tai Chen <mindlikewater@gmail.com>
2016-10-23 17:36:18 -0400

    Added a click event for when user selects a song

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,28 @@
 import $ from 'jquery';
 
-import { getTracks } from "./soundcloud_api";
-import { songTemplate, resultsTemplate, playMusic } from "./templates";
+import { getTracks } from './soundcloud_api';
+import { songTemplate, resultsTemplate, play } from './templates';
 
-//variable holds location of the div, search-results from HTML page
+//variable holds location of the div, search-results from webpage
 var resultsBox = $(".search-results");
 
-//function calls functions that create songs HTML and then displays the results on the webpage
+//calls functions that create songs HTML and then displays the results on the webpage
+//wires a click event whenever user selects a song to the music player
 function displayResults (results) {
   var playlist = results.map(songTemplate);
-  var html = resultsTemplate(playlist);
+  var resultsHTML = resultsTemplate(playlist);
 
-  resultsBox.html(html);
+  resultsBox.html(resultsHTML);
+  $(".songbox").click(play);
 };
 
-//gets user input from the page, then runs another function to get search results
+//gets user input from the page, then runs another function to generate search results
 function getMusic (event) {
   event.preventDefault();
   var searchString = $("#search").val();
   var results = getTracks(searchString);
 
+  console.log(searchString);
   console.log(results);
   results.then(displayResults);
 };
